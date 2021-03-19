1b61db017167c262e7da09fa910cc6ef369bdb01
iwgx <wahabiputra@gmail.com>
2021-03-19 22:19:38 +0700

    Remove Logo and links in homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">tech-web</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">Bersihin Tech</h1>
     </div>
   </div>
 </template>
