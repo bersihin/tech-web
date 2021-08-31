18734a088311b759b2a38a20b6399dacfbb5ebed
iwgx <wahabiputra@gmail.com>
2021-08-31 08:45:20 +0700

    Update meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,33 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Introduce Bersihin technology team to the world by explain ourself, share what we done and what we learned, our event, also opportunity to work with us' },
+      { name: 'keywords', content: 'laundry,online,offline,onsite,subscription,delivery,bandung,standard,bersihin,tech,flutter,firebase,vue,nuxt,typescript,dart' },
+
+      // Title
+      { property: 'og:title', content: 'Bersihin Tech' },
+      { name: 'twitter:title', content: 'Bersihin Tech' },
+      { name: 'application-name', content: 'Bersihin Tech' },
+      { name: 'apple-mobile-web-app-title', content: 'Bersihin Tech' },
+
+      // Description
+      { name: "description", content: "Introduce Bersihin technology team to the world by explain ourself, share what we done and what we learned, our event, also opportunity to work with us" },
+      { property: 'og:description', content: 'Introduce Bersihin technology team to the world by explain ourself, share what we done and what we learned, our event, also opportunity to work with us' },
+      { name: "twitter:description", content: "Introduce Bersihin technology team to the world by explain ourself, share what we done and what we learned, our event, also opportunity to work with us" },
+
+      // Image
+      { property: 'og:image', content: 'https://i.ibb.co/VWP7QbD/bersihintech-thumbnail.jpg' },
+      { name: 'twitter:image', content: 'https://i.ibb.co/VWP7QbD/bersihintech-thumbnail.jpg' },
+
+      // URL
+      { rel: "canonical", href: "https://tech.bersihin.co" },
+      { property: "og:url", content: "https://tech.bersihin.co" },
+      { name: "twitter:url", content: "https://tech.bersihin.co" },
+
+      { property: 'og:type', content: 'website' },
+      { property: 'og:site_name', content: 'bersihin' },
+
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:creator", content: "@bersihin_tech" },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
